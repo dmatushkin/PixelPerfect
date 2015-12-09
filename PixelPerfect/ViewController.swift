c95d0d18869e37a2b728309be9878a5f09e14f1d
Dmitry Matyushkin <dmatushkin@gmail.com>
2015-12-09 13:50:05 +0300

    Initial Commit

---
+++ PixelPerfect/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PixelPerfect
+//
+//  Created by Dmitry Matyushkin on 12/9/15.
+//  Copyright © 2015 Dmitry Matyushkin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
